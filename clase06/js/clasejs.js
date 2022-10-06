bc5e996733b2249e19818341b6728c81443ee7d2
MatiasMV <mjmazavega@gmail.com>
2022-10-06 18:06:56 -0300

    principio de JS y variables

---
+++ clase06/js/clasejs.js
@@ -0,0 +1,62 @@
+//Variables
+
+console.log('Hola JavaScript');
+
+var pepe; //declaracion de una variable
+
+let nombre;
+
+const DNI = 40641420;
+
+nombre = 'Matias' 
+
+console.log(nombre);
+
+nombre = 'Javier' 
+nombre = 'Zioshi'
+
+console.log(nombre);
+
+console.log(DNI);
+
+let falso = false;
+
+let verdadero = true;
+
+console.log(falso);
+
+console.log(verdadero);
+
+console.log('========================================================================================');
+
+
+//Mostrar datos en la consola
+
+//concatenar datos
+console.log("Hola, mi nombre es " + nombre + " y mi país es Argentina y mi DNI es " + DNI + ".");
+
+console.log('Hola, mi nombre es ' + nombre + ' y mi país es Argentina y mi DNI es ' + DNI + '.');
+
+//utilizamos el template string
+console.log(`Hola, mi nombre es ${nombre} y mi país es Argentina y mi DNI es ${DNI}.`);
+
+console.log('========================================================================================');
+
+//Arrays
+
+let banana = 'banana'
+let manzana = 'manzana'
+
+let frutas = ['peras','bananas','manzanas','naranjas'];
+
+console.log(`Las frutas del arreglo son ${frutas}`)
+
+let persona = ['Pepe','1245789','true'];
+
+console.log(`Los datos del arreglo son ${persona}`)
+
+console.log('========================================================================================');
+
+//Objetos
+
+
