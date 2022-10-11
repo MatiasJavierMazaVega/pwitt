3e62b329eb11d9cc5d13a1097e9d555804af5844
MatiasMV <mjmazavega@gmail.com>
2022-10-11 18:01:37 -0300

    continuacion js y principio ejercicio Bootstrap

--- clase06/js/clasejs.js
+++ clase06/js/clasejs.js
@@ -51,12 +51,38 @@ let frutas = ['peras','bananas','manzanas','naranjas'];
 
 console.log(`Las frutas del arreglo son ${frutas}`)
 
+console.log(`La primer fruta de nuestro array es: ${frutas[0]}`)
+console.log(`La primer fruta de nuestro array es: ${frutas[1]}`)
+console.log(`La primer fruta de nuestro array es: ${frutas[2]}`)
+console.log(`La primer fruta de nuestro array es: ${frutas[3]}`)
+
+//Metodo de los arreglos
+console.log(`Los elementos del array frutas son ${frutas.length} `)
+
+frutas.push('mangos')
+
+console.log(`Las frutas del arreglo son ${frutas}`)
+
+console.log(`Los elementos del array frutas son ${frutas.length} `)
+
 let persona = ['Pepe','1245789','true'];
 
 console.log(`Los datos del arreglo son ${persona}`)
 
 console.log('========================================================================================');
 
-//Objetos
+//Objetos - JSON
 
+let pais = {
+    nombre: 'Argentina' ,
+    provincias: 24 ,
+    capital: 'Buenos Aires' ,
+    poblacion: 45000000 ,
+    ubicacion: 'America' , 
+}
 
+console.log(pais);
+console.log(`La capital de mi pais es ${pais.capital}`)
+console.log(`La capital de mi pais es ${pais.nombre}`)
+console.log(`La capital de mi pais es ${pais.poblacion}`)
+console.log(`La capital de mi pais es ${pais.provincias}`)
--- clase06/js/index.js
+++ clase06/js/index.js
@@ -1,1 +1,14 @@
 console.log('Hola Mundo');
+
+alert('Bienvenido al sistema')
+
+let dato = ['Pepe'];
+
+let guardarDato = prompt('Introduce tu nombre: ');
+
+dato.push(guardarDato);
+
+alert('Bienvenido ' + guardarDato)
+
+console.log(`Los nombres son: ${dato}`)
+
