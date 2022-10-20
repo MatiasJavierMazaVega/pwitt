fcc1cad86e69496204d97622562bce6ee70ebd34
MatiasMV <mjmazavega@gmail.com>
2022-10-20 18:06:49 -0300

    mas JS y condicionales

--- clase09/js/index.js
+++ clase09/js/index.js
@@ -15,26 +15,82 @@ function saludar() {
 }
 
 function iniciar() {
-    numeroUno = prompt('Ingrese un número');
-    numeroDos = prompt('Ingrese otro número');
+    
+    /*
+    numeroUno = parseFloat(prompt('Ingrese un número: '));
+    numeroDos = parseFloat(prompt('Ingrese otro número: '));
+    */
+    datoUno = prompt('Ingrese un número: ');
+    datoDos = prompt('Ingrese otro número: ');
+
+    numeroUno = parseFloat (datoUno);
+    numeroDos = parseFloat (datoDos);
+
     console.log(numeroUno, numeroDos);
 }
 
+/*function validar() {
+    if (numeroUno == NaN && numeroDos == NaN) {
+        alert('Es necesario llenar el dato')
+    } 
+}*/
 
+/*function validar() {
+    if (numeroUno == undefined && numeroDos == undefined ) {
+        alert('Es necesario llenar el dato, click en iniciar Calculos')
+    } 
+}*/
+
+/*
+function validar(){
+    if(isNaN(numero1) || isNaN(numero2))
+    {
+        alert('Debe ingresar un valor numérico')
+    }
+    else if(numero1 == "" || numero2 == "")
+    {
+        alert('Debe completar ambos valores')
+    }
+}
 
 function sumar() {
-    suma = numeroUno + numeroDos;
-    console.log(' El resultado de la suma es ' + suma)
+    validar();
+    suma = numero1 + numero2;
+    resultado.innerHTML = `El resultado es: ${suma}`;
+} */
+
+function sumar() {
+
+    //validacion con un if
+    //true/false
+    //validar();
+    if (numeroUno == undefined && numeroDos == undefined ) {
+        alert('Es necesario llenar el dato, click en Iniciar Calculos')
+    } else{
+        suma = numeroUno + numeroDos;
+        resultado.innerHTML = `El resultado de la suma es: ${suma}`;
+        console.log(' El resultado de la suma es ' + suma)
+    }
+
+    
+
+
 }
 
 function restar() {
-    
+    //validar();
+    resta = numeroUno - numeroDos;
+    resultado.innerHTML = `El resultado de la resta es: ${resta}`;
 }
 
 function multiplicar() {
-    
+    //validar();
+    multiplicacion = numeroUno * numeroDos;
+    resultado.innerHTML = `El resultado de la multiplicacion es: ${multiplicacion}`;
 }
 
 function dividir() {
-    
+    //validar();
+    division = numeroUno / numeroDos;
+    resultado.innerHTML = `El resultado de la division es: ${division}`;
 }
